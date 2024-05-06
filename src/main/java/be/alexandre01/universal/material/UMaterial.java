3cad6f18898681bd0244978eae1657c3cb7453a4
Alexandre Taillet <alexandre.taillet@student.vinci.be>
2024-05-06 13:52:32 +0200

    Remove nms on pom

--- src/main/java/be/alexandre01/universal/material/UMaterial.java
+++ src/main/java/be/alexandre01/universal/material/UMaterial.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UMaterial  {
+
     public static final ModernMaterial modern = new ModernMaterial();
 
     public static final UItemData AIR = new UItemData("air", "air");
